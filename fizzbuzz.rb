4afc80ac0a25a5e6ed94a63b2ee9b6e82de5a1c2
Eddie Fisher <eddief28@tiscali.co.uk>
2017-10-29 05:15:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(num)
   remainder1 = num % 3
   remainder2 = num % 5
-  
+
     if remainder1 == 0 && remainder2 == 0
       puts "FizzBuzz"
     end
@@ -17,4 +17,4 @@ end
 else
   return nil
 end
-end  
+end
